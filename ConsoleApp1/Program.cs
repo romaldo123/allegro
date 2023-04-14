d56ef155322a0060271f52e26efe34a9e75b7eea
rvmus <rvmus@DESKTOP-2FGSAGF>
2023-04-15 00:10:44 +0200

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
